c4fe09c52ab5911a070460a4c83f3e9499ed502c
Jacob Coblammers <jacob.coblammers@readytalk.com>
2016-08-25 03:13:46 -0600

    Get rid of annoying paredit key binding.

---
+++ lisp/init-local.el
@@ -0,0 +1,8 @@
+;; My customizations go here
+;;; Code;
+
+(global-set-key (kbd "M-}") 'forward-paragraph)
+(global-set-key (kbd "M-{") 'backward-paragraph)
+
+;; provide init local should be the last thing in this file
+(provide 'init-local)
